439560e9172fe29e14297849e5663f2b833a5a38
Matthew Jutrzonka <jutrzont@gmatc.matc.edu>
2023-05-04 13:40:50 -0500

    Two Screen Navigation

--- MEALS/components/CategoryGridTile.js
+++ MEALS/components/CategoryGridTile.js
@@ -1,6 +1,6 @@
 import { Pressable, View, Text, StyleSheet, Platform } from "react-native";
 
-function CategoryGridTile({ title, color }) {
+function CategoryGridTile({ title, color, onPress }) {
     return (
         <View style={styles.gridItem}>
             <Pressable
@@ -9,6 +9,7 @@ function CategoryGridTile({ title, color }) {
                     styles.button,
                     pressed ? styles.buttonPressed : null,
                 ]}
+                onPress={onPress}
             >
                 <View style={[styles.innerContainer, { backgroundColor: color }]}>
                     <Text style={styles.title}>{title}</Text>
@@ -18,9 +19,9 @@ function CategoryGridTile({ title, color }) {
     );
 }
 
-    export default CategoryGridTile;
+export default CategoryGridTile;
 
-    const styles = StyleSheet.create({
+const styles = StyleSheet.create({
     gridItem: {
         flex: 1,
         margin: 16,
