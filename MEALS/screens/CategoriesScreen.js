b8e2682f1875ca8914eef821bca96892617abc62
Matthew Jutrzonka <jutrzont@gmatc.matc.edu>
2023-05-04 14:34:44 -0500

    Finished MEALS App

--- MEALS/screens/CategoriesScreen.js
+++ MEALS/screens/CategoriesScreen.js
@@ -4,30 +4,30 @@ import CategoryGridTile from "../components/CategoryGridTile";
 import { CATEGORIES } from "../data/dummy-data";
 
 function CategoriesScreen({ navigation }) {
-    function renderCategoryItem(itemData) {
-        function pressHandler() {
-            navigation.navigate("MealsOverview", {
-                categoryId: itemData.item.id,
-            });
-        }
-
-        return (
-            <CategoryGridTile
-                title={itemData.item.title}
-                color={itemData.item.color}
-                onPress={pressHandler}
-            />
-        );
+  function renderCategoryItem(itemData) {
+    function pressHandler() {
+      navigation.navigate("MealsOverview", {
+        categoryId: itemData.item.id,
+      });
     }
 
     return (
-        <FlatList
-            data={CATEGORIES}
-            keyExtractor={(item) => item.id}
-            renderItem={renderCategoryItem}
-            numColumns={2}
-        />
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
     );
+  }
+
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
 }
 
 export default CategoriesScreen;
